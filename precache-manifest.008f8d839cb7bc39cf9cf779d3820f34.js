48d4ea7aca5fa8cc93300d4a931bf4df06921845
felibuscaglia <felipebenjamin1810@gmail.com>
2021-02-18 10:02:35 -0300

    Updates

---
+++ precache-manifest.008f8d839cb7bc39cf9cf779d3820f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d9aa734d42f1e97aa9731d69f9859a",
+    "url": "/movieSearcher/index.html"
+  },
+  {
+    "revision": "0f22e1829bb73bec95ea",
+    "url": "/movieSearcher/static/css/main.3f00e6e0.chunk.css"
+  },
+  {
+    "revision": "85a48eb37f882e38f203",
+    "url": "/movieSearcher/static/js/2.76a6ceec.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movieSearcher/static/js/2.76a6ceec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f22e1829bb73bec95ea",
+    "url": "/movieSearcher/static/js/main.b968ceb9.chunk.js"
+  },
+  {
+    "revision": "2df591a62b3e9a915264",
+    "url": "/movieSearcher/static/js/runtime-main.d01fbea2.js"
+  }
+]);
